65c4d3a6016f04f78956455632997245fa07bf3b
volp99 <stefanovolpatti@yahoo.it>
2021-12-01 11:57:09 +0100

    end section 5 (dynamic styling)

---
+++ Theory/05-basics-dynamic-styling/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			boxA: false,
+			boxB: false,
+			boxC: false,
+		}
+	},
+	computed: {
+		boxAClasses() {
+			return {active: this.boxA}
+		},
+	},
+	methods: {
+		boxSelected(box) {
+			if (box === 'A') {
+				this.boxA = !this.boxA
+			} else if (box === 'B') {
+				this.boxB = !this.boxB
+			} else if (box === 'C') {
+				this.boxC = !this.boxC
+			}
+		},
+	},
+
+})
+app.mount('#styling')
